52fa23421b3f03ad94e02c24862022f87884f226
Julio <ju.crr2@gmail.com>
2025-03-09 17:14:09 -0300

    Teste CSS JS

---
+++ funcs.js
@@ -0,0 +1,17 @@
+function openMenu() {
+    document.getElementById("menu_aba").style.display = "block"; 
+  }
+  
+  function closeMenu() {
+    document.getElementById("menu_aba").style.display = "none";    
+  }
+
+  function temaInatel() {
+    document.documentElement.style.setProperty('--cor-click', '#126ae2');
+    document.documentElement.style.setProperty('--cor-sombra', '#0a599b');
+    document.documentElement.style.setProperty('--cor-text', 'black');
+    document.documentElement.style.setProperty('--cor-back1', '#edf2f4');
+    document.documentElement.style.setProperty('--cor-back2', '#6a937a');
+    document.documentElement.style.setProperty('--md-sys-color-primary', '#126ae2');
+  
+}  
