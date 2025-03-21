58a31d1ef622ed86c21dffc343cbaf332c4dcce9
Julio <ju.crr2@gmail.com>
2025-03-21 09:30:45 -0300

    Carousel 1.0

--- funcs.js
+++ funcs.js
@@ -144,3 +144,40 @@ document.addEventListener("DOMContentLoaded", function(){ // 'document' Acessa d
 
     });
 });
+
+// Controle do carrossel
+let index = 0;
+function nextCard() {
+    index = (index + 1) % eventos.length;
+    updateCarousel();
+}
+
+function prevCard() {
+    index = (index - 1 + eventos.length) % eventos.length;
+    updateCarousel();
+}
+
+function updateCarousel() {
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+}
+
+// Adicionando interatividade
+document.getElementById('nextBtn').addEventListener('click', nextCard);
+document.getElementById('prevBtn').addEventListener('click', prevCard);
+
+// Auto avanço a cada 5 segundos
+setInterval(nextCard, 5000);
+
+// Arrastar no celular
+let startX;
+carousel.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX;
+});
+carousel.addEventListener('touchend', (e) => {
+    let endX = e.changedTouches[0].clientX;
+    if (startX - endX > 50) nextCard();
+    if (endX - startX > 50) prevCard();
+});
+
+// Inicializando o carrossel
+createCards();
